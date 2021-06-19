2067283f9072ccbfdf5fa90478a8dd216ccc4d35
Ahmed Shahriar Tanvir <shhrrtnvr@gmail.com>
2021-06-19 21:49:15 +0600

    "Graham Scan added"

--- geo_complex.cpp
+++ geo_complex.cpp
@@ -45,6 +45,37 @@ bool isConvex(vector<pt> p) {
     }
     return !(hasNeg && hasPos);
 }
+//Graham Scan
+void convex_hull(vector<pt>& a) {
+    if (a.size() == 1)
+        return;
+
+    sort(a.begin(), a.end(), [](pt a, pt b) {
+        return a.x < b.x || (a.x == b.x && a.y < b.y);
+    });
+    pt p1 = a[0], p2 = a.back();
+    vector<pt> up, down;
+    up.push_back(p1);
+    down.push_back(p1);
+    for (int i = 1; i < (int)a.size(); i++) {
+        if (i == a.size() - 1 || orient(p1, a[i], p2) < 0) {
+            while (up.size() >= 2 && !(orient(up[up.size()-2], up[up.size()-1], a[i]) < 0))
+                up.pop_back();
+            up.push_back(a[i]);
+        }
+        if (i == a.size() - 1 || orient(p1, a[i], p2) > 0) {
+            while(down.size() >= 2 && !(orient(down[down.size()-2], down[down.size()-1], a[i]) > 0))
+                down.pop_back();
+            down.push_back(a[i]);
+        }
+    }
+
+    a.clear();
+    for (int i = 0; i < (int)up.size(); i++)
+        a.push_back(up[i]);
+    for (int i = down.size() - 2; i > 0; i--)
+        a.push_back(down[i]);
+}
 
 //Polar Sort 
 bool half(pt p) {
